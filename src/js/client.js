860a434408aed93007959084bed62cc983bfa2fa
error25 <autovincelynch@gmail.com>
2016-06-05 13:06:00 +0100

    react initialised

---
+++ src/js/client.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//import Layout from "./components/Layout";
+
+class Layout extends React.Component {
+  render(){
+    return (
+     <h1>its working!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  context: __dirname,
+  context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js", //this is important
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
   output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
+    path: __dirname + "/src/",
+    filename: "client.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
