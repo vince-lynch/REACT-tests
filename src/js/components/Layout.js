597e28ae431a763ae4146e96a8ed0acbb34bade1
error25 <autovincelynch@gmail.com>
2016-06-05 14:37:38 +0100

    trying out different solutions for two way data binding in react

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
+
+changeTitle(title){
+  this.setState({title});
+}
 
   render(){
     return (
      <div>
-      <Header />
+     <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+     <Footer />
      </div>
     );
   }
